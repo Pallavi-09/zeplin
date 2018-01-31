a7137c9b75186f4be818d860c3e100b218b60dc1
Pallavi Singh <pallavisingh992@gmail.com>
2018-01-31 17:33:49 +0530

    commit

--- header1/script/header.js
+++ header1/script/header.js
@@ -1,4 +1,7 @@
-  $('#nav-bar-icon').on('click', function(){
-        $('#show-nav').toggleClass('nav-bar-subicon');
-    });
+$('#nav-bar-icon').on('click', function(){
+    $('#show-nav').toggleClass('nav-bar-subicon');
+    $('#cross-icon').addClass('display-block');
+    $('.bar').addClass('display_none');
+});
+
  

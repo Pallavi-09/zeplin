032662eba27fee560d56c439250acd5d91819223
Pallavi Singh <pallavisingh992@gmail.com>
2018-02-20 09:58:47 +0530

    c

--- header1/script/header.js
+++ header1/script/header.js
@@ -1,33 +1,37 @@
+var currentState = true;
+var labelState = true;
 $('#nav-bar-icon').on('click', function(){
     $('#show-nav').toggleClass('nav-bar-subicon');
     $('#cross-icon').toggleClass('display-block');
     $('.bar').toggleClass('display_none');
 });
 $('#user-option').on('click', function(){
-    if(this.currentState === true){
+    if(currentState === true){
         $('#user-option i').removeClass().addClass('fa fa-angle-up down-icon fa-2x ');
     }
     else{
         $('#user-option i').removeClass().addClass('fa fa-angle-down down-icon fa-2x ');
     }
-    this.currentState = !this.currentState;
+    currentState = !currentState;
 });
 $('#alert-icon').on('click', function(){
-    
-    console.log(1);
     var x = ($(this).position().left)-244;
     $('.alert-icon').css('left',x);
-    if(this.labelState === true){
-        $(this).attr('src','assets/images/header/active_notification.png');
+    if(labelState === true){
+        $(this).attr('src','images/active_notification.png');
     }
     else{
-        $(this).attr('src','assets/images/header/alert.png');
+        $(this).attr('src','images/alert.png');
     }
-    this.labelState = !this.labelState;
+    labelState = !labelState;
 });
 
 $('#user-option').on('click', function(e){
     var x = ($(this).position().left)+(-233);
     $('.user-option-dropdown').css('left',x);
 });
- 
+
+window.onresize = function() {
+    var x = ($(this).position().left)+(-23);
+    $('.user-option-dropdown').css('left',x);
+}

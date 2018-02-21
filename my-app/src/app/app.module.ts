5b1333e914c799703b3663e658be68636473899b
Pallavi Singh <pallavisingh992@gmail.com>
2018-02-21 17:16:55 +0530

    c

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -7,19 +7,22 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailsComponentComponent } from './hero-details-component/hero-details-component.component';
 import { HeroService } from './hero.service';
+import { MessageServiceService } from './message-service.service';
+import { MessagesComponent } from './messages/messages.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailsComponentComponent
+    HeroDetailsComponentComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [HeroService],
+  providers: [HeroService, MessageServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- my-app/src/app/hero.service.ts
+++ my-app/src/app/hero.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { observable } from 'rxjs/observable';
+import { Observable } from 'rxjs/Observable';
+import { MessageServiceService } from './message-service.service';
 import { of } from 'rxjs/Observable/of';
 
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageServiceService) { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes():Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
   }
 
 }

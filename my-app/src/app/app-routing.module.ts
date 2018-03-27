7deec4162af8817d7dad1c672964b74bbd6c3345
Pallavi Singh <pallavisingh992@gmail.com>
2018-03-27 10:46:12 +0530

    commiting changes

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -4,19 +4,51 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { HeroDetailsComponentComponent } from './hero-details-component/hero-details-component.component';
+import { CanActivate } from "@angular/router";
+import { Injectable } from "@angular/core";
 
+@Injectable()
+class AlwaysAuthGuard implements CanActivate {
+  canActivate() {
+      console.log("AlwaysAuthGuard Running");
+      return true;
+    }
+}
 
+class UserService {
+  isLoggedIn() : boolean {
+    return false;
+  }
+}
+
+@Injectable()
+class OnlyLoggedInGuard implements CanActivate {
+  
+  constructor(private userservice : UserService){}
+
+  canActivate(){
+      if(this.userservice.isLoggedIn()){
+        return true;
+      }
+      else{
+        alert("Sorry ! You don't have permission");
+        return false;
+      }
+  }
+}
 const routes: Routes = [
   { path:'', component: WelcomeComponent },  
-  { path:'heroes', component: HeroesComponent },
+  { path:'heroes',canActivate:[OnlyLoggedInGuard,AlwaysAuthGuard], component: HeroesComponent },
   { path:'dashboard', component: DashboardComponent },
   { path:'detail/:id', component: HeroDetailsComponentComponent}
 ];
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers:[OnlyLoggedInGuard,UserService,AlwaysAuthGuard]
 })
+
 
 export class AppRoutingModule { 
 
--- my-app/src/app/hero.service.ts
+++ my-app/src/app/hero.service.ts
@@ -4,15 +4,28 @@ import { HEROES } from './mock-heroes';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';  // URL to web api
 
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
+  }
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 
+  getHero(id: number): Observable<Hero> {
+    // Todo: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }

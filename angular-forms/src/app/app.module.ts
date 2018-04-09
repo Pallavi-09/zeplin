2e8b083ff04a8126a4fb65c25776881446d10645
Pallavi Singh <pallavisingh992@gmail.com>
2018-04-09 17:07:54 +0530

    working on forms

--- angular-forms/src/app/app.module.ts
+++ angular-forms/src/app/app.module.ts
@@ -1,31 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import { HeroFormComponent } from './hero-form/hero-form.component';
-import { CarComponent } from './car/car.component';
-import { MessageService } from './car/employee.service';
-import { HttpClientModule } from '@angular/common/http';
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
-import { ProductDetailComponent } from './product/product-detail/product-detail.component'; 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroFormComponent,
-    CarComponent,
-    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule,
-    Ng2SearchPipeModule
   ],
-  providers: [MessageService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

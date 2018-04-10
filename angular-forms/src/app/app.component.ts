5d64935664ce6f180723a223358f7b0179e71d95
Pallavi Singh <pallavisingh992@gmail.com>
2018-04-10 16:45:55 +0530

    working form

--- angular-forms/src/app/app.component.ts
+++ angular-forms/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee } from './employee.model';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   languages:any[] = ["Hindi","English","Spanish","German"];
+
+  // constructor( public firstname:string, public lastname:string){}
+
+  // employee(firstname,lastname){
+  //     return "FirstName->"+firstname+" Lastname->" + lastname;
+  // }
+  model = new Employee('', 'Singh', true, '1099','German');
+  // onChangeModel(e){
+  //   if(e.length > 0 ){
+  //     this.model.firstname = e.charAt(0).toUpperCase()+e.slice(1);
+  //     console.log(e);
+  //   }
+  //   else{
+  //     this.model.firstname = e;
+  //   }
+  // }
 }
---
+++ angular-forms/src/app/employee.model.ts
@@ -0,0 +1,12 @@
+
+export interface IEmployee{
+    firstname:string,
+    lastname:string,
+    fullcheck:boolean,
+    paymentType:string,
+    lang:string
+}
+
+export class Employee implements IEmployee{
+   constructor(public firstname:string, public lastname:string, public fullcheck:boolean, public paymentType:string, public lang:string) {}
+} 
